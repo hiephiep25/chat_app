e71c0b15717b4184cfd384d128bee3d752071b8f
hiephiep <117154647+hiephiep25@users.noreply.github.com>
2023-04-14 10:39:58 +0700

    dynamic users and search bar

--- users.php
+++ users.php
@@ -1,22 +1,29 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Chat App</title>
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
-</head>
+<?php 
+    session_start();
+    if(!isset($_SESSION['unique_id'])){
+        header("location: login.php");
+    }
+?>
+
+<?php 
+    include_once "header.php";
+?>
 <body>
     <div class="wrapper">
         <section class="users">
             <header>
+                <?php 
+                    include_once "php/config.php";
+                    $sql = mysqli_query($conn, "SELECT * FROM users WHERE unique_id = {$_SESSION['unique_id']}" );
+                    if(mysqli_num_rows($sql) > 0){
+                        $row = mysqli_fetch_assoc($sql);
+                    }
+                ?>
                 <div class="content">
-                    <img src="img/hiep.jpg" alt="">
+                    <img src="php/images/<?php echo $row['img'] ?>" alt="">
                     <div class="details">
-                        <span>Hiep hiep</span>
-                        <p>Active now</p>
+                        <span><?php echo $row['fname'] . " " . $row['lname'] ?></span>
+                        <p><?php echo $row['status'] ?></p>
                     </div>
                 </div>
                 <a href="#" class="logout">Logout</a>
@@ -29,26 +36,7 @@
                 <button><i class="fas fa-search"></i></button>
             </div>
             <div class="users-list">
-                <a href="">
-                    <div class="content">
-                        <img src="img/hiep.jpg" alt="">
-                        <div class="details">
-                            <span>Hiep hiep</span>
-                            <p>This is test message</p>
-                        </div>
-                    </div>
-                    <div class="status-dot"><i class="fas fa-circle"></i></div>
-                </a>
-                <a href="">
-                    <div class="content">
-                        <img src="img/hiep.jpg" alt="">
-                        <div class="details">
-                            <span>Hiep hiep</span>
-                            <p>This is test message</p>
-                        </div>
-                    </div>
-                    <div class="status-dot"><i class="fas fa-circle"></i></div>
-                </a>
+                
             </div>
         </section>
     </div>
